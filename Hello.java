5f581a388f3aecd436372f464713e8f4dc9d3019
Karthik Shetty <45932358+karthikshetty03@users.noreply.github.com>
2019-11-10 02:42:28 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,34 @@
+import javax.swing.*;  
+class Hello  
+{  
+public static void main(String args[])  
+    {  
+	    JFrame f= new JFrame("User Registration"); 
+	    
+	    JTextField t1;
+	    JPasswordField t2; 
+	    JLabel l1,l2;  
+	    
+	    l1=new JLabel("Username");  
+	    l1.setBounds(50,50, 100,30); 
+	    
+	    t1=new JTextField(" ");  
+	    t1.setBounds(50,75, 200,30); 
+	    
+	    l2=new JLabel("Password");  
+	    l2.setBounds(50,125, 100,30); 
+	    
+	    t2=new JPasswordField();  
+	    t2.setBounds(50,150, 200,30);
+	    
+	    JButton b=new JButton("Register");  
+	    b.setBounds(50,200,95,30); 
+	    
+	    f.add(l1); f.add(l2);
+	    f.add(t1); f.add(t2); 
+	    f.add(b);
+	    f.setSize(400,400);  
+	    f.setLayout(null);  
+	    f.setVisible(true);  
+    }  
+ } 
