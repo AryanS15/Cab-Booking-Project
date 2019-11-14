522fa201c86271c2f58f6788a43cd65afe923ef1
Karthik Shetty <45932358+karthikshetty03@users.noreply.github.com>
2019-11-15 05:23:34 +0530

    Update Option.java

--- Option.java
+++ Option.java
@@ -5,22 +5,27 @@ import java.util.ArrayList;
 import java.util.logging.Logger;
 import java.sql.*;
 public class Option
-{    
+{ 
+	public String e,b,c;
+public float fr;
+public int t;
+public int dist;
 JFrame f;  
-JLabel l1,l2,l3,l4;
+JLabel l1,l2,l3,l4,l5,l6,l7,l8,l9,l10;
 JComboBox<Object> cb,ed;
+public static float amount;
 Connection con=DBConnect.getConnection();
-String point[]={"BPHC","Secunderabad Railway Station","Thumkunta","Airport","Parade Ground"}; 
-Option(){    
+String point[]={"BPHC","Secunderabad_Railway_Station","Thumkunta","Airport","Begumpet"}; 
+public Option(float wallet){    
     f=new JFrame("Select a route");           
- 
+    amount = wallet;
     l1=new JLabel("From : ");
     l1.setBounds(50,25,250,20); 
 	
 	cb= new JComboBox<Object>(point);    
     cb.setBounds(50, 50,250,20);
     
-    
+    try {
 	cb.addActionListener(new ActionListener() {
     public void actionPerformed(ActionEvent actionEvent) {
     	System.out.println(cb.getSelectedItem());
@@ -46,7 +51,11 @@ Option(){
     }
   });
   
-    
+    }
+    catch(Exception e)
+    {
+    	
+    }
     
     
     JButton b= new JButton("Proceed");
@@ -55,12 +64,31 @@ Option(){
 		
 		public void actionPerformed(ActionEvent ae) {
 			
-			details(((JLabel) cb.getSelectedItem()).getText(),((JLabel) ed.getSelectedItem()).getText());
-			//calcFareTime(distance);
-			//l3=new JLabel("Distance : "+distance);
-			//l3.setBounds(100,100,250,20);
-			//f.add(l3);
-			
+			details(cb.getSelectedItem().toString(),ed.getSelectedItem().toString());
+			System.out.println("DIstance : "+dist+" km");
+			if(dist==0)
+			{
+				JOptionPane.showMessageDialog(null, "Please select different source/destination");
+			}
+			else
+			{
+				l3=new JLabel("Congratulations! Your Trip is confirmed");
+				l3.setBounds(210,150,300,30);
+				f.add(l3);
+				dispDistCalcTimeAndFare(dist);
+				
+				JButton b1=new JButton("Begin Trip");
+				b1.setBounds(275,500,200,50);
+				f.add(b1);
+				b1.addActionListener(new ActionListener() {
+					
+					public void actionPerformed(ActionEvent ae) {
+						
+				new MyTimer(5,amount,fr,e);
+					}
+				});
+
+			}
 		}
 	});
 	
@@ -74,9 +102,7 @@ Option(){
     f.setVisible(true);    
     
 
-      
-    //code for point != point condition
-      findDriver(cb.getSelectedIndex());
+  
 }    
 public void findDriver(int a) {
 	try {
@@ -92,20 +118,27 @@ public void findDriver(int a) {
 			
 			if(result.next())
 			{
-				System.out.println(result.getString("Name"));
-				   
+				e=result.getString("Name");
+				b=result.getString("Rating");
+				c=result.getString("Mobile");
 			}
 			else
-			{
+			{	
 				//driver with highest rating from min distance location
-				System.out.println("Sorry ! No drivers");
+				//NoDriver();
 			}
 		}
 		catch(Exception e)
 		{
-			System.out.println(e);
+			System.out.println("errrroooorrrrr"+e);
 		}
+	
 }
+//public void NoDriver()
+//{
+//	String query="SELECT * FROM drivers WHERE Location='"+(a+1)+"' AND Rating=(SELECT MAX(Rating) FROM drivers WHERE Location = '"+(a+1)+"')";
+//}
+//
 
 public void details(String object1,String object2)  //cb.getSelectedItem() is the source and ed.getSelectedItem() is the destination
 {
@@ -113,39 +146,56 @@ public void details(String object1,String object2)  //cb.getSelectedItem() is th
 	{
 		System.out.println(object1+" "+object2);
 		
-		System.out.println("Hello");
+		
 			
-	 String query = "SELECT Airport FROM final WHERE Source='Parade Ground' ";
+	 String query = "SELECT "+object1+" FROM final WHERE Source='"+object2+"'";
 	 PreparedStatement statement3=con.prepareStatement(query);
 		
 		ResultSet result=statement3.executeQuery(query);
-		int d=result.getInt("Airport");
-		System.out.println("Helo"+d);
+		while(result.next())
+		{
+			String d=result.getString(object1);
+			dist=Integer.parseInt(d);
+		}
+		
+		
 			
 			
 	}
 	catch(Exception e)
 	{
+		System.out.println("errrroooorrrrr "+e);
 		
 	}
 	
+	
+	
 }
-	public void dispDistCalcTime(int d)
+
+	public void dispDistCalcTimeAndFare(int d)
 	{
-		//float f=20*d;
-		int t=10*d;
-		//label j4 and j5 should print the distance and time required for the journey
+		fr=3*d;
+		t=4*d;
 		
-		new MyTimer(t);
-		//new Details(f,wallet);
-		
-		
+		l4=new JLabel("Duration of the trip :"+t+" minutes");
+		l5=new JLabel("Fare for the trip : Rs. "+fr);
+		l6=new JLabel("Distance of the trip : "+d+" km");
+		l7=new JLabel("Name of driver :"+e);
+		l8=new JLabel("Rating of driver :"+b);
+		l9=new JLabel("Mobile No. of driver :"+c);
+		l7.setBounds(210,200,300,30);
+		l8.setBounds(210,225,300,30);
+		l9.setBounds(210,250,300,30);
+		l6.setBounds(210,350,300,30);
+		l4.setBounds(210,375,300,30);
+		l5.setBounds(210,400,300,30);
+		f.add(l4); f.add(l5); f.add(l6); f.add(l7); f.add(l8); f.add(l9);
 		
 		
 		
 	}
 	
 public static void main(String[] args) {    
-    new Option();         
+    new Option(1000);         
 }    
 } 
